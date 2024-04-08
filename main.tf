323c802c68c04611a5f25921fd7b4dac671751f1
Assaf Dori <assafxdori@gmail.com>
2024-04-08 12:42:31 +0300

    cleaned up terraform files

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "us-east-1"  # Replace with your desired region
-}
-
-resource "aws_instance" "web_server" {
-  ami           = "ami-0440d3b780d96b29d"  # Replace with your desired AMI ID
-  instance_type = "t2.micro"               # Replace with your desired instance type
-}
--- outputs.tf
+++
@@ -1,3 +0,0 @@
-output "server_public_ip" {
-  value = aws_instance.web_server.public_ip
-}
