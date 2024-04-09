0188973710573a8c39fa8ec9e5d780d3c42ca1fd
Assaf Dori <assafxdori@gmail.com>
2024-04-09 20:12:12 +0300

    added key

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -3,6 +3,11 @@ resource "aws_instance" "resume-app-ec2-instance" {
   instance_type = "t4g.nano"
   subnet_id     = aws_subnet.resume-app-public-subnet.id
   security_groups = [aws_security_group.resume-app-security-group.id]
+  key_name = "ssh-key-resume-server"
+
+  tags = {
+    Name = "resume-app-server"
+  }
 }
 
 output "ec2_instance_ip" {
