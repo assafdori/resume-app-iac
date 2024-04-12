1965df3731bd64f98148487cee40856a18430d39
Assaf Dori <assafxdori@gmail.com>
2024-04-12 12:18:09 +0300

    added update-ns.py script

--- update-ns.py
+++ update-ns.py
@@ -16,19 +16,24 @@ client = boto3.client('secretsmanager', region_name=region_name)
 # Retrieve Porkbun API credentials from AWS Secret Manager
 response = client.get_secret_value(SecretId=secret_name)
 secret_dict = json.loads(response['SecretString'])
-pork_api_key = secret_dict['pork_api_key']
-pork_api_secret = secret_dict['pork_api_secret']
+pork_api_key = secret_dict['pork_api_key']  # Ensure this key name matches what's in Secrets Manager
+pork_api_secret = secret_dict['pork_api_secret']  # Ensure this key name matches what's in Secrets Manager
 
 # Initialize a Route 53 client
 route53_client = boto3.client('route53')
 
 # Retrieve the hosted zone ID for the domain
 response = route53_client.list_hosted_zones_by_name(DNSName=domain_name)
-hosted_zone_id = response['HostedZones'][0]['Id'].split('/')[-1]  # Extracting the hosted zone ID
+for hosted_zone in response['HostedZones']:
+    if hosted_zone['Name'] == f"{domain_name}.":
+        hosted_zone_id = hosted_zone['Id'].split('/')[-1]  # Extracting the hosted zone ID
+        break
+else:
+    raise ValueError("Hosted zone not found for domain.")
 
 # Retrieve name servers from Route 53
 response = route53_client.get_hosted_zone(Id=hosted_zone_id)
-name_servers = [ns['Value'] for ns in response['DelegationSet']['NameServers']]
+name_servers = response['DelegationSet']['NameServers'].split(',')  # Split string into a list
 
 # Update DNS records on Porkbun
 headers = {
