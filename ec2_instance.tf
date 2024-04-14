aaae1e26f81072900ab74287610ecb55e8ef5485
Assaf Dori <assafxdori@gmail.com>
2024-04-14 13:24:26 +0300

    minor

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "resume-app-ec2-instance" {
   ami             = var.aws_ami # This is a custom AMI. ARM platform with Docker pre-installed.
   instance_type   = "t4g.nano"
-  subnet_id       = aws_subnet.resume-app-public-subnet.id
   security_groups = [aws_security_group.resume-app-security-group.id]
+  subnet_id       = aws_subnet.resume-app-public-subnet.id
   key_name        = "ssh-key-resume-server"
 
   tags = {
