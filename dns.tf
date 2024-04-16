c64313d8709aab6f940e5e6e189df4ba3325733c
Assaf Dori <assafxdori@gmail.com>
2024-04-16 12:47:09 +0300

    troubleshooting

--- dns.tf
+++ dns.tf
@@ -15,8 +15,6 @@ resource "aws_route53_record" "www" {
     zone_id                = aws_lb.resume-app-application-load-balancer.zone_id
     evaluate_target_health = true
   }
-
-  
 }
 
 resource "aws_route53_record" "root" {
