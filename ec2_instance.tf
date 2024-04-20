56a458c3533b33eef79707204e110231a9f84ba9
Assaf Dori <assafxdori@gmail.com>
2024-04-20 21:14:50 +0300

    changed ec2 type

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "resume-app-ec2-instance" {
   ami             = var.aws_ami # This is a custom AMI. ARM platform with Docker pre-installed.
-  instance_type   = "t4g.nano"
+  instance_type   = "t4g.micro"
   security_groups = [aws_security_group.resume-app-security-group.id]
   subnet_id       = aws_subnet.resume-app-public-subnet.id
   key_name        = "ssh-key-resume-server"
