86909ca9157a933352209e225e84d04082f8330b
Assaf Dori <assafxdori@gmail.com>
2024-05-26 11:00:44 +0300

    modified tf outputs

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,10 @@
 output "ec2_instance_ip" {
+  description = "Public IP of the EC2 instance"
   value = aws_instance.resume-app-ec2-instance.public_ip
 }
 
 output "dns_records" {
-  description = "DNS records created"
+  description = "DNS records"
   value = {
     www  = aws_route53_record.www.fqdn
     root = aws_route53_record.root.fqdn
@@ -11,5 +12,6 @@ output "dns_records" {
 }
 
 output "acm_certificate_arn" {
+  description = "ACM certificate ARN"
   value = aws_acm_certificate.resume-app-cert.arn
 }
