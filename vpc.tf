747eb883784c839eb3497790fa1ddc3f1b2691ff
Assaf Dori <assafxdori@gmail.com>
2024-05-26 10:43:04 +0300

    added vpc tag

--- vpc.tf
+++ vpc.tf
@@ -2,4 +2,8 @@ resource "aws_vpc" "resume-app-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "resume-app-vpc"
+  }
 }
