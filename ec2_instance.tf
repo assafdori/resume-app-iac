62227ad713ffebb8b84efa313276884e2de31c54
Assaf Dori <assafxdori@gmail.com>
2024-06-18 23:49:28 +0300

    https://stackoverflow.com/a/75080425/23570160 - reference

--- ec2_instance.tf
+++ ec2_instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "resume-app-ec2-instance" {
   ami             = var.aws_ami # This is a custom AMI. ARM platform with Docker pre-installed.
   instance_type   = var.aws_instance_type
-  security_groups = [aws_security_group.resume-app-security-group.id]
+  vpc_security_group_ids = [aws_security_group.resume-app-security-group.id]
   subnet_id       = aws_subnet.resume-app-public-subnet.id
   key_name        = var.aws_key_pair
 
